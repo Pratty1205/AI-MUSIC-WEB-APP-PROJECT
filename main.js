167d5f008ea68304001e1adb0ffdb0571263ba24
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-08-31 22:26:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+grateful = "";
+wma = "";
+
 function setup() {
     canvas = createCanvas(600, 450);
     video = createCapture(VIDEO);
-    canvas.center()
+    canvas.center();
+    video.hide();
+}
+
+function preload() {
+    grateful = loadSound("Grateful.mp3");
+    wma = loadSound("We'll Meet Again.mp3")
+}
+
+function draw() {
+    image(video, 0, 0, 600, 450);
 }
