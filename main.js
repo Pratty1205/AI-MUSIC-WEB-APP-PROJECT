bf670490a24bc6f4d7de783555d8fa53356f585e
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-09-02 22:39:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 grateful = "";
 wma = "";
+LeftX = 0;
+LeftY = 0;
+RightX = 0;
+RightY = 0;
 
 function setup() {
     canvas = createCanvas(600, 450);
     video = createCapture(VIDEO);
     canvas.center();
     video.hide();
+    posenet = ml5.poseNet(video, model_loaded);
+    posenet.on("pose", getResult);
+}
+
+function model_loaded() {
+    console.log("Model is not loaded");
 }
 
 function preload() {
@@ -15,4 +25,14 @@ function preload() {
 
 function draw() {
     image(video, 0, 0, 600, 450);
+}
+
+function getResult(result) {
+    if (result.length > 0) {
+        console.log(result);
+        LeftX = result[0].pose.leftWrist.x;
+        LeftY = result[0].pose.leftWrist.y;
+        RightX = result[0].pose.rightWrist.x;
+        RightY = result[0].pose.rightWrist.y;
+    }
 }
